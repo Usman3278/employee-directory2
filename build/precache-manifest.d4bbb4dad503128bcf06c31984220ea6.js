950a539de6867af82750f26ec6e7feb7d7486c6e
Usman3278 <usman3278@gmail.com>
2021-04-02 10:07:33 -0400

    modified package.json

---
+++ build/precache-manifest.d4bbb4dad503128bcf06c31984220ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027e988a167c09e37059f5329d7b4797",
+    "url": "/employee-directory2/index.html"
+  },
+  {
+    "revision": "72392174c2207b143c29",
+    "url": "/employee-directory2/static/css/main.ac93ded3.chunk.css"
+  },
+  {
+    "revision": "377a4bd9e4f2158e4c01",
+    "url": "/employee-directory2/static/js/2.f853ab81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory2/static/js/2.f853ab81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72392174c2207b143c29",
+    "url": "/employee-directory2/static/js/main.42dd73f3.chunk.js"
+  },
+  {
+    "revision": "1d7ead0bd0de8f1a2ac2",
+    "url": "/employee-directory2/static/js/runtime-main.dfb283a9.js"
+  }
+]);
